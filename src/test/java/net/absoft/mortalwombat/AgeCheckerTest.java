6a57fe043be780b65a29eeb0ec18f3fbdcabb037
Obitotski <andrew.obitotski@gmail.com>
2022-04-07 15:03:47 +0300

    AgeChecker Tests

---
+++ src/test/java/net/absoft/mortalwombat/AgeCheckerTest.java
@@ -0,0 +1,19 @@
+package net.absoft.mortalwombat;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AgeCheckerTest {
+
+    @Test
+    public void testAgedUserCanPlay(){
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertTrue(ageChecker.canPlayGame(19), "Aged user can`t play game");
+    }
+
+    @Test
+    public void testThatTooYoungUserCanNotPlay(){
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertFalse(ageChecker.canPlayGame(16),"Too young user can play game");
+    }
+}
